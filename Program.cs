bd4abd546637f8f895aaff7f69a430659ab97380
Stephanie Tahova <106015292+sstephanie7@users.noreply.github.com>
2022-05-30 18:50:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); 
+
+            for (int number = 1; number <= n; number++)
+            {
+                int sumOfDigits = 0;
+                int digits = number;
+
+                while (digits != 0)
+                {
+                    sumOfDigits += digits % 10;
+                    digits /= 10; 
+                }
+
+                if (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11)
+                {
+                    Console.WriteLine($"{number} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{number} -> False");
+                }
+            }
+        }
+    }
+}
